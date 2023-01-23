25e01baf3fb519901e306856d4f9b1f01782e9d2
cyberbuddy-manas <rspmanas1@gmail.com>
2023-01-23 21:57:34 +0530

    app born

--- js/main.js
+++ js/main.js
@@ -1,84 +1,108 @@
+
 (function ($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+    /*==================================================================
+    [ Validate ]*/
+    // var input = $('.validate-input .input100');
+
+    // $('.validate-form').on('submit',function(){
+    //     var check = true;
+
+    //     for(var i=0; i<input.length; i++) {
+    //         if(validate(input[i]) == false){
+    //             showValidate(input[i]);
+    //             check=false;
+    //         }
+    //     }
+
+    //     return check;
+    // });
 
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    // $('.validate-form .input100').each(function(){
+    //     $(this).focus(function(){
+    //        hideValidate(this);
+    //     });
+    // });
+
+    // function validate (input) {
+    //     if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+    //         if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    //             return false;
+    //         }
+    //     }
+    //     else {
+    //         if($(input).val().trim() == ''){
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    // function showValidate(input) {
+    //     var thisAlert = $(input).parent();
+
+    //     $(thisAlert).addClass('alert-validate');
+    // }
+
+    // function hideValidate(input) {
+    //     var thisAlert = $(input).parent();
+
+    //     $(thisAlert).removeClass('alert-validate');
+    // }
 
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
 
+    /*==================================================================
+    [ Simple slide100 ]*/
 
-    // Date and time picker
-    $('.date').datetimepicker({
-        format: 'L'
-    });
-    $('.time').datetimepicker({
-        format: 'LT'
-    });
+    $('.simpleslide100').each(function () {
+        var delay = 7000;
+        var speed = 1000;
+        var itemSlide = $(this).find('.simpleslide100-item');
+        var nowSlide = 0;
 
+        $(itemSlide).hide();
+        $(itemSlide[nowSlide]).show();
+        nowSlide++;
+        if (nowSlide >= itemSlide.length) { nowSlide = 0; }
 
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        loop: true,
-        nav: false,
-        dots: true,
-        items: 1,
-        dotsData: true,
-    });
-
-
-    // Testimonials carousel
-    $('.testimonial-carousel').owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        loop: true,
-        nav: false,
-        dots: true,
-        items: 1,
-        dotsData: true,
+        setInterval(function () {
+            $(itemSlide).fadeOut(speed);
+            $(itemSlide[nowSlide]).fadeIn(speed);
+            nowSlide++;
+            if (nowSlide >= itemSlide.length) { nowSlide = 0; }
+        }, delay);
     });
 
-    
+
 })(jQuery);
 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+    document.getElementById("vid").pause();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById("vid").pause();
+    }
+}
